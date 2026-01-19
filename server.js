8eaf72c258a91ff2d5c3aa62402e368ec0117850
JackApfel <94872960+JackApfel@users.noreply.github.com>
2026-01-19 08:48:16 +0100

    renamed:    Style/style.css -> public/css/style.css 	renamed:    scripts/what -> public/js/what 	new file:   server.js 	renamed:    Index.html -> view/Index.html 	renamed:    Login.html -> view/Login.html 	renamed:    Monats-Berichte.html -> view/Monats-Berichte.html 	renamed:    Tagesbericht.html -> view/Tagesbericht.html 	renamed:    Wochenberichte.html -> view/Wochenberichte.html 	renamed:    dev.html -> view/dev.html

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.json());
+
+
+
+
+// Das MUSS am Ende bleiben:
+app.get((req, res) => {
+    // 404 handler
+    res.status(404).send('Ressource nicht gefunden');
+})
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
