343b58492f635a773903fec92fe286aed9e3d675
JackApfel <94872960+JackApfel@users.noreply.github.com>
2026-01-19 13:12:14 +0100

    modified:   .gitignore 	modified:   package.json 	new file:   prisma.config.ts 	new file:   prisma/schema.prisma 	renamed:    server.js -> server.cjs 	modified:   view/Login.html 	new file:   view/Register.html     Added Prisma to Project and created User Model     Created Login and Register Pages

--- server.cjs
+++ server.cjs
@@ -3,12 +3,23 @@ const app = express();
 const path = require('path');
 const port = 3000; //test port
 
+//generated\prisma\client.ts
+const { PrismaClient } = require(path.join(__dirname,'generated', 'prisma', 'client.ts'));
+
 // für json body parsing
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'view', 'index.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'Register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'Login.html'));
 });
 
 
