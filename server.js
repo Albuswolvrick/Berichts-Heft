33e0a27a9832c46dd4e6f4c3ca98f1354e5ed98c
JackApfel <94872960+JackApfel@users.noreply.github.com>
2026-02-02 11:49:03 +0100

    modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = 3000; //test port
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
